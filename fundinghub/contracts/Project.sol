2624cc757d15460f1dd220694412fcba0ef1b4b6
Adan <bitcoinplus2@gmail.com>
2016-08-28 19:00:57 -0500

    Update Project.sol

--- fundinghub/contracts/Project.sol
+++ fundinghub/contracts/Project.sol
@@ -1,60 +1,121 @@
-contract PROJECT {
+//updgrade by peer-to peer.experience. I can´t connect the rng with this.the fundinghub that send //for submission
+contract Project {
 
-
-
-  uint constant  contributePeriod = 100 days;
-
-  contribute[] public contributions;
-
+  struct Campaign{
+  uint constant  contributePeriod = 10 days;
+  uint constant targetAmount = 100000000 wei;
   address public curator;
+  string title;
+  uint deadline;
+  bytes32 contributionsHash;
+}
+  address public hub;
+  address public benificiary;
+  uint public status;
+  ProjectInfo public info;
+  mapping(address => uint)public allowedRecipientsOfContributions;
+  bool public active;
+  bool public refundable;
+  bool public successful;
+  / Prevents methods from perfoming any value transfer
+  modifier noEther() { if (msg.value > 0) throw; _}
 
-  mapping(address = > bool)public allowedRecipientsOfContributions;
+  modifier isActive() {if(!active)throw; _}
 
+  modifier isRefundable() {if(!refundable) throw;_}
 
-  mapping (address => uint) public FINDINGHUBpaidOut;
+  modifier blankAddress(address _n) { if (_n != 0) throw; _}
 
-  mapping
+  modifier moreThanZero(uint256 _deposit) { if (_deposit <= 0) throw; _}
 
-  uint sumOfContributionsDeposits;
+  modifier beFalse(bool _t) { if (_t) throw; _}
 
-  // contract to create a new FINDINGHUB (avoid retrancy bugs,TOD , blocktime dependece.)
-
-  FINDINGHUB_Creator public findighubcreator;
-
-
+  modifier isHub() {  if(msg.sender != hub) throw;  _}
 }
 
-  struct contributions {
-    address recipient;
-    uint amount ;
+  struct Contributtor {
 
+    uint amount ;
+    uint index;
     string Data ;
     string Description;
-    uint  Deadline;
     bytes32 contributionsHash; // hash, to  validity contribution
+  }
+  mapping (address => Contributor) contributorInfo;
+  address[] public contributors;
+  event refundFailed(address contributor);
 
-
+  function Project(address _curator, string _title,string _contributionsHash, uint _targetAmount, uint _deadline) {
+    hub = msg.sender;
+    campaign = Campaign(tx.origin, _amount, _deadline, _title);
+    curator: _curator,
+    title: _title,
+  });
+}
+modifier onlyHub() {  if (msg.sender != hub) throw;  _}
+function contributionOf(address a) constant returns(uint) {
+    return contributorInfo[a].amount;
   }
 
-  function fund()
+  function fund(address source) onlyHub {
+    if (now <= info.deadline) {
+      if (msg.value > 0) {
+        uint amount = info.targetAmount - collectedFunds; // targetAmount is always >= of collectedFunds
+        if (msg.value > amount) {
+          // returns the change
+          if (!source.send(msg.value - amount)) throw;
+        } else {
+          amount = msg.value;
+        }
+        collectedFunds += amount;
+        Contributor cData = contributorInfo[source];
+        if (cData.amount == 0)
+          cData.index = contributors.push(source);
+        cData.amount += amount;
+        // do payout if the target is reached. The check is >= but the collected funds never exceeds the target
+        if (collectedFunds >= info.targetAmount) payout();
+      }
+    } else {
+      // fundraising period expired
+      if (msg.value > 0) {
+        if (!source.send(msg.value)) throw;
+      }
+      // after the deadline the balance may not be >= of targetAmount, should refund contributors
+      if (this.balance > 0) refund();
+    }
+  }
+
+  function payout() internal{
+        if (beneficiary.send(amountRaised)) {
+            FundTransfer(beneficiary, amountRaised, false);
+        } else {
+            //If we fail to send the funds to beneficiary, unlock funders balance
+            fundingGoalReached = false;
+        }
+
+	}
+
+
+  // This function sends all individual contributions back to the respective contributor.
+  function refund() internal{
+    if (status == 0) {
+      status = 2;
+      uint contributorsCount = contributors.length;
+      for (uint i=0; i < contributorsCount; i++) {
+        address target = contributors[i];
+        Contributor c = contributorInfo[target];
+        if (!target.send(c.amount)) {
+          // ignore transfer errors because all untrasfered funds will be forwarded
+          // to the hub to be handled by the maintainer
+          refundFailed(target);
+        }
+      }
+      // forward untrasfered ethers to the hub to be handled by the maintainer
+      if (this.balance > 0)
+        hub.call.value(this.balance);
+    }
+  }
+  function () {
+    throw;
+  }
 }
-
-
-
-
-
-
-
-
-
-fund()
-
-
-
-
-payout()
-
-
-
-
-refund()
